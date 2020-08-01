7bc21bd2f08c806cb072aa395825e4fc125cc979
Jorge Braz <jorge.braz@intelectah.com.br>
2020-08-01 00:20:10 -0300

    Construção

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -2,21 +2,85 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Contador de Passos",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+  runApp(MaterialApp(title: "Contador de Passos", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _pessoas = 0;
+  String _infText = "Pode Entrar!";
+
+  void _changePessoa(int valor) {
+    setState(() {
+      _pessoas += valor;
+
+      if (_pessoas < 0) {
+        _infText = "Mundo Invertido?";
+      } else if (_pessoas <= 10) {
+        _infText = "Pode Entrar!";
+      }
+      if (_pessoas > 10) {
+        _infText = "Lotado!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: <Widget>[
-        Text(
-          "Pessoas: 0",
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000,
         ),
-        Text(
-          "Pode Entrar: 0",
-          style: TextStyle(
-              color: Colors.black, fontStyle: FontStyle.italic, fontSize: 30.0),
-        )
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: $_pessoas",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: FlatButton(
+                    child: Text("+1",
+                        style: TextStyle(fontSize: 40, color: Colors.white)),
+                    onPressed: () {
+                      _changePessoa(1);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: FlatButton(
+                    child: Text("-1",
+                        style: TextStyle(fontSize: 40, color: Colors.white)),
+                    onPressed: () {
+                      _changePessoa(-1);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              _infText,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        ),
       ],
-    ),
-  ));
+    );
+  }
 }
