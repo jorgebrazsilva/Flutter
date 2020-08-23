56dfe6c2083347a143e6c37cd7de58d78b4fe3b0
Jorge Braz <jorge.braz@intelectah.com.br>
2020-08-23 02:39:03 -0300

    Configure Firebase

--- chat_flutter/lib/main.dart
+++ chat_flutter/lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(Container());
+  Firestore.instance
+      .collection("col")
+      .document("doc")
+      .setData({"texto": "Jorge"});
 }
